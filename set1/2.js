7c99899f33f45f0497ea2841388f22615cbd3fa2
leonh <hello@leonh.fr>
2018-09-10 16:04:33 +0200

    feat: challenge 2

---
+++ set1/2.js
@@ -0,0 +1,15 @@
+const a = '1c0111001f010100061a024b53535009181c';
+const b = '686974207468652062756c6c277320657965';
+const c = '746865206b696420646f6e277420706c6179';
+
+const xor = a
+  .hexDecode()
+  .xor(b.hexDecode())
+  .hexEncode();
+
+module.exports = function () {
+  console.log('a        ', a);
+  console.log('b        ', b);
+  console.log('c = a ^ b', c);
+  console.log('a ^ b    ', xor);
+}
--- utils/data.js
+++ utils/data.js
@@ -1,7 +1,20 @@
+// String methods
+
 String.prototype.hexDecode = function () {
   return Buffer.from(this, 'hex');
 }
 
+// Buffer methods
+
+Buffer.prototype.hexEncode = function () {
+  return this.toString('hex');
+}
+
 Buffer.prototype.base64Encode = function () {
   return this.toString('base64');
+}
+
+Buffer.prototype.xor = function (key) {
+  return this
+    .map((x, i) => (x ^ key[i % key.length]));
 }
