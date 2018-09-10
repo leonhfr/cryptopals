c6f216c3566e0542f581cb978e5243a7a91d4ae5
leonh <hello@leonh.fr>
2018-09-10 16:44:02 +0200

    feat: reading data and passing it to challenges

--- index.js
+++ index.js
@@ -6,30 +6,45 @@ const path = require('path');
 const utils  = require('./utils/data.js');
 const crypto = require('./utils/crypto.js');
 
-const challenge = Number(process.argv[2]) || 1;
-if (typeof challenge !== 'number' ||
-    !Number.isInteger(challenge) ||
-    challenge < 1 ||
-    challenge > 64) {
+let files;
+const data = {};
+try {
+  files = fs.readdirSync(path.join(__dirname, 'data'));
+} catch (err) {
+  throw new Error('Data directory does not exit:', err );
+}
+files.forEach(file => {
+  const fileId = file.replace('.txt', '');
+  const fileData = fs.readFileSync(path.join(__dirname, 'data', file));
+
+  data[fileId] = fileData;
+})
+
+const id = Number(process.argv[2]) || 1;
+if (typeof id !== 'number' ||
+    !Number.isInteger(id) ||
+    id < 1 ||
+    id > 64) {
   throw new Error('Challenge is out of bound.');
 }
 
-const set      = Math.ceil(challenge / 8);
+const set      = Math.ceil(id / 8);
 const setDir   = `set${set}`;
-const fileName = `${challenge}.js`;
+const fileName = `${id  }.js`;
 
-let files;
+let challenges;
 try {
-  files = fs.readdirSync(path.join(__dirname, setDir));
+  challenges = fs.readdirSync(path.join(__dirname, setDir));
 } catch (err) {
   throw new Error('Set directory does not exit:', err );
 }
 
-const file = files.find(file => file === fileName);
-if (!file) {
+const challengeExists = challenges.find(file => file === fileName);
+if (!challengeExists) {
   throw new Error('Challenge has not been completed yet / file does not exist.')
 }
 
-console.log(`***  Challenge ${challenge}  ***`);
-const challengePath = path.join(__dirname, setDir, fileName)
-require(challengePath)();
+console.log(`***  Challenge ${id}  ***`);
+const challenge = path.join(__dirname, setDir, fileName)
+
+require(challenge)(data);
