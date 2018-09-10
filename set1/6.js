be99cff9874dfe5b0a229daacc8944a9d577e87a
leonh <hello@leonh.fr>
2018-09-10 19:27:36 +0200

    feat: challenge 6

--- set1/6.js
+++ set1/6.js
@@ -1,4 +1,4 @@
-module.exports = () => {
+module.exports = (data) => {
   console.log('Break repeating-key XOR\n');
 
   const a    = 'this is a test';
@@ -12,5 +12,60 @@ module.exports = () => {
   console.log('b: ', b);
   console.log(`should be 37 and was found to be ${dist}\n`);
 
+  const ciphertext = data.base64Decode();
+  const keyLengths = findKeyLengths(ciphertext);
 
+  console.log('Key lengths:\n');
+  keyLengths.forEach(a => {
+    console.log('Hamming distance: ', a.distance.toFixed(1), ' | Key length: ', a.length);
+  });
+  console.log('\n');
+
+  const sol = breakXOR(ciphertext, keyLengths[0].length)
+
+  console.log('Key:', sol.key.toAscii(), '\n');
+  console.log('Plaintext:\n', sol.plaintext.toAscii());
+}
+
+function findKeyLengths (ciphertext) {
+  const nBlocks  = 10;
+  let keyLengths = [];
+
+  for (let length = 2; length <= 40; length++) {
+    keyLengths.push({
+      length,
+      distance: testKeySize(ciphertext, length, nBlocks)
+    });
+  }
+
+  return keyLengths
+    .sort((a, b) => a.distance - b.distance)
+    .slice(0, 3);
+}
+
+function testKeySize (ciphertext, length, nBlocks) {
+  const blocks = ciphertext.getBlocks(length, nBlocks);
+
+  let distance = 0;
+  blocks.reduce((acc, curr) => {
+    distance += acc.hammingDistance(curr);
+    return curr;
+  });
+
+  return distance / length;
+}
+
+function breakXOR (ciphertext, keyLength) {
+  const blocks = ciphertext
+    .getBlocks(keyLength)
+    .transposeBlocks();
+
+  const keyBytes = blocks.map(x => x.singleCharacterXor().key);
+  const key      = Buffer.from(keyBytes);
+
+  return {
+    keyLength,
+    key,
+    plaintext: ciphertext.xor(key)
+  };
 }
--- utils/data.js
+++ utils/data.js
@@ -34,3 +34,35 @@ Buffer.prototype.bits = function () {
     .replace(/0/g, '')
     .length;
 }
+
+Buffer.prototype.getBlocks = function (length, nBlocks) {
+  let blocks = [];
+
+  if (nBlocks === undefined) nBlocks = Math.ceil(this.length / length);
+
+  for (let i = 0; i < nBlocks; i++) {
+    let index = i * length;
+    blocks.push(this.slice(index, index + length));
+  }
+
+  return blocks;
+}
+
+// Array methods
+
+Array.prototype.transposeBlocks = function () {
+  const length = this[0].length;
+  let transposed = [];
+
+  for (let i = 0; i < length; i++) {
+    let block = [];
+
+    for (let k = 0; k < this.length; k++) {
+      block.push(this[k][i]);
+    }
+
+    transposed.push(Buffer.from(block));
+  }
+
+  return transposed;
+}
