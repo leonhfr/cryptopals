092bfb739376a89efa7fb13cbb5fdfcbdc31a0e8
leonh <hello@leonh.fr>
2018-09-11 11:55:17 +0200

    feat: challenge 10 part 2

--- set2/10.js
+++ set2/10.js
@@ -1,24 +1,36 @@
 module.exports = (data) => {
   console.log('Implement CBC mode\n');
 
-  const plaintext = 'Oh well, whatever, nevermind';
-  const key       = 'Kurt Cobain Flea'
-  const ecbTest   = plaintext
+  const test    = 'Oh well, whatever, nevermind';
+  const key     = 'YELLOW SUBMARINE'
+  const ecbTest = test
     .asciiDecode()
     .pad()
     .ecbEncrypt(key)
     .ecbDecrypt(key)
     .stripPadding()
     .asciiEncode()
-    === plaintext;
-  const cbcTest   = plaintext
+    === test;
+  const cbcTest = test
     .asciiDecode()
     .cbcEncrypt(key)
     .cbcDecrypt(key)
     .stripPadding()
     .asciiEncode()
-    === plaintext;
+    === test;
+
+  const iv = [ 0,0,0,0,
+               0,0,0,0,
+               0,0,0,0,
+               0,0,0,0 ];
+  const plaintext = data
+    .replace(/\n/g, '')
+    .base64Decode()
+    .cbcDecrypt(key, iv)
+    .stripPadding()
+    .asciiEncode();
 
   console.log('ECB test: ', ecbTest);
   console.log('CBC test: ', cbcTest);
+  console.log('Plaintext:\n', plaintext);
 }
