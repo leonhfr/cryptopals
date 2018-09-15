a23c000c2562f3f94c4911809c804621e489bc75
leonh <hello@leonh.fr>
2018-09-15 13:27:01 +0200

    fix: correct pkcs7 padding

--- utils/pad.js
+++ utils/pad.js
@@ -1,21 +1,19 @@
 Buffer.prototype.pad = function (blockLength) {
   blockLength = blockLength || 16;
-  const padLength = blockLength - this.length % blockLength;
-  if (blockLength === 0 || padLength <= 0) return this;
+  const padLength = blockLength - this.length % blockLength || blockLength;
   return Buffer.concat([this, Buffer.alloc(padLength, padLength)]);
 }
 
 Buffer.prototype.stripPadding = function (blockLength) {
   blockLength = blockLength || 16;
   const pad = this[this.length - 1];
-  if (pad < blockLength && this.valPadding(blockLength))
+  if (pad <= blockLength && this.valPadding(blockLength))
     return this.slice(0, this.length - pad);
   else return this;
 }
 
 Buffer.prototype.valPadding = function (blockLength) {
   blockLength = blockLength || 16;
-  // if (this.length % blockLength !== 0) return false;
   const pad = this[this.length - 1];
   for (let i = 0; i < pad; i++) {
     if (this[this.length - i - 1] !== pad) return false;
