f40e8ebac3340fe8a0c76c8b63d9b4ce4adfa98d
leonh <hello@leonh.fr>
2018-09-15 13:15:31 +0200

    feat: challenge 16

--- set2/16.js
+++ set2/16.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const assert = require('assert');
 
 module.exports = () => {
   console.log('CBC bitflipping attack\n');
@@ -6,45 +7,91 @@ module.exports = () => {
   // DEFINE KEY AND CIPHER
 
   const key      = crypto.randomBytes(16);
-  const cipher   = function (input) {
-    return encryptURL(input, key);
-  }
-  const decipher = function (input) {
-    return decryptURL(input, key);
-  }
+  const iv       = crypto.randomBytes(16);
+  const cipher   = {
+    encrypt: (input) => {
+      const prefix  = 'comment1=cooking%20MCs;userdata=';
+      const suffix  = ';comment2=%20like%20a%20pound%20of%20bacon';
+      return (prefix + input + suffix).asciiDecode().cbcEncrypt(key, iv);
+    },
+    decrypt: (input) => {
+      return input.cbcDecrypt(key, iv)
+    }
+  };
 
   // FUNCTIONS TESTS
 
-  const test1 = !decryptURL(encryptURL('hello', key), key);
-  const test2 = !decryptURL(encryptURL('data;admin=true', key), key);
-  const test3 = !decryptURL(encryptURL('', key, 'dataadmin=true'), key);
-  const test4 = decryptURL(encryptURL('', key, 'data;admin=true'), key);
-  console.log('Test basic functions:');
-  console.log((test1 && test2 && test3 && test4) ? 'PASS' : 'FAIL');
+  console.log('0: test basic functions');
+  assert.ok(!'hello'.encryptURL(cipher.encrypt).testURL(cipher.decrypt));
+  assert.ok(!'data;admin=true'.encryptURL(cipher.encrypt).testURL(cipher.decrypt));
+  assert.ok(!'dataadmin=true'.encryptURL(cipher.encrypt).testURL(cipher.decrypt));
+  assert.ok(!'data;admin=true'.encryptURL(cipher.encrypt).testURL(cipher.decrypt));
 
-  
+  // GET TARGET BLOCK
+  const targetBlock = cipher.encrypt.findTargetBlock();
+  console.log('1: get target block:');
+  console.log('  ', targetBlock);
+
+  // BREAK IT
+  console.log('2: break it!');
+  const target = ';admin=true;';
+  const broke  = cipher.breakCBC(targetBlock, target);
+  console.log(broke.stripPadding().asciiEncode());
 }
 
-const encryptURL = function (input, key, test) {
-  const prefix  = 'comment1=cooking%20MCs;userdata=';
-  const suffix  = ';comment2=%20like%20a%20pound%20of%20bacon';
-  const quoted  = test ?
-    test :
-    input.replace(/;/g, '%3B').replace(/=/g, '%3D');
-  const data    = (prefix + quoted + suffix).asciiDecode();
-  // random iv is used, will only affect the first block
-  const iv      = crypto.randomBytes(16);
-  const discard = crypto.randomBytes(16);
-  return Buffer
-    .concat([discard, data])
-    .cbcEncrypt(key, iv);
+Object.prototype.breakCBC = function (targetBlock, target) {
+  const ciphertext  = target.encryptURL(this.encrypt);
+  const blocks      = ciphertext.getBlocks();
+  const mutateBlock = targetBlock - 1;
+  console.log('Original ciphertext:\n' + ciphertext.hexEncode());
+  console.log('Original ciphertext:\n' + this.decrypt(ciphertext).stripPadding().asciiEncode());
+  let index = 0;
+  let lastIndex = -1;
+  while (!Buffer.concat(blocks).testURL(this.decrypt, target)) {
+    if (index > lastIndex) lastIndex = index;
+    else throw new Error('could not find byte', index)
+    for (let i = 0; i < 256; i++) {
+      blocks[mutateBlock] = blocks[mutateBlock].changeByte(index, [i]);
+      const testBlock     = this
+        .decrypt(Buffer.concat(blocks))
+        .getBlocks()
+        [targetBlock];
+      if (testBlock[index] === Buffer.from(target)[index]) {
+        console.log('found byte', index);
+        index++;
+        break;
+      }
+    }
+  }
+  const plaintext = Buffer.concat(blocks);
+  console.log('Ciphertext after bitflipping attack:\n' + plaintext.hexEncode());
+  return this.decrypt(plaintext);
 }
 
-const decryptURL = function (input, key) {
-  // we discard the first decrypted block
-  return input
-    .cbcDecrypt(key)
-    .slice(16)
-    .asciiEncode()
-    .search(/;admin=true;/g) > 0;
+String.prototype.encryptURL = function (cipher) {
+  const data = this.replace(/;/g, '%3B').replace(/=/g, '%3D');
+  return cipher(data);
+}
+
+Buffer.prototype.testURL = function (cipher, target) {
+  return cipher(this).asciiEncode().search(new RegExp(target, 'g')) > 0;
+}
+
+Function.prototype.findTargetBlock = function () {
+  const stringA = Buffer.alloc(16, 'A').asciiEncode();
+  const stringB = Buffer.alloc(16, 'A').changeByte(0, 'B').asciiEncode();
+  const bufferA = this(stringA).getBlocks();
+  const bufferB = this(stringB).getBlocks();
+  for (let i = 0; i < bufferA.length; i++) {
+    if (!bufferA[i].equals(bufferB[i])) return i
+  }
+  return -1;
+}
+
+Buffer.prototype.changeByte = function (index, byte) {
+  return Buffer.concat([
+    this.slice(0, index),
+    Buffer.from(byte),
+    this.slice(index + 1)
+  ]);
 }
