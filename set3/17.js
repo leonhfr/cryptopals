923e3dea10ac5b73345a1e9ec59e0bbb4d95c51d
leonh <hello@leonh.fr>
2018-09-18 12:14:42 +0200

    feat: challenge 17 progress

--- set3/17.js
+++ set3/17.js
@@ -5,21 +5,73 @@ module.exports = (data) => {
 
   const key      = crypto.randomBytes(16);
   const iv       = crypto.randomBytes(16);
-  data = data
+  const target = data
     .split('\n')
     [Math.floor(Math.random() * 10)]
-    .base64Decode();
+    .base64Decode()
+    .cbcEncrypt(key, iv);
 
-  console.log(data);
+  const apiEndpoint = (ciphertext) => ciphertext.cbcDecrypt(key, iv).valPadding();
+  const blockLength = 16;
+  const plaintext   = target
+    .paddingOracleCBC(apiEndpoint, blockLength)
+    .stripPadding()
+    .asciiEncode();
+  console.log(plaintext);
+}
 
-  const randomCBC = function (data) {
-    return this
-      .pad()
-      .cbcEncrypt(key, iv);
+Buffer.prototype.paddingOracleCBC = function (apiEndpoint, blockLength) {
+  // this = target ciphertext
+  const blocks = this.getBlocks(blockLength);
+  const iv     = blocks[0] || blocks.shift();
+  console.log('iv:', iv);
+  console.log('blocks:');
+  console.log(blocks);
+  let plaintext = [];
+  for (let i = 1; i < blocks.length; i++) {
+    plaintext.push(blocks[i-1].paddingOracleCBCBlock(apiEndpoint, blockLength, blocks[i]));
   }
-  const isPaddingValid = function (data) {
-    return this
-      .cbcDecrypt(key, iv)
-      .valPadding();
+  return Buffer.concat(plaintext);
+}
+
+Buffer.prototype.paddingOracleCBCBlock = function (apiEndpoint, blockLength, target) {
+  // this = ciphertext block c1
+  // target = ciphertext block c2
+  let intermState = Array(blockLength).fill(Buffer.from([0]));
+  for (let i = 1; i <= blockLength; i++) {
+    // console.log('*** Trying to find byte', i);
+    const intermBuffer = Buffer.concat(intermState);
+    const intermByte   = intermBuffer
+      .paddingOracleCBCByte(apiEndpoint, blockLength, target, i);
+    intermState[blockLength - i] = intermByte;
+    if (intermByte < 0) throw new Error('Could not find byte ' + i);
   }
+  console.log('intermState', intermState);
+  return intermState;
+}
+
+Buffer.prototype.paddingOracleCBCByte = function (apiEndpoint, blockLength, target, padding) {
+  // this = intermediate state block I2
+  // target: ciphertext block C2
+  // padding: 1-16
+
+  const paddingTarget = Buffer.from([padding]);
+  const suffixData    = [];
+  for (let i = 1; i < padding; i++) {
+    suffixData.unshift(paddingTarget.xor(Buffer.from([this[blockLength - i]])));
+  }
+  const suffix = Buffer.concat(suffixData);
+  const prefix = Buffer.alloc(blockLength - padding);
+  for (let i = 0; i < 256; i++) {
+    const byte       = Buffer.from([i]);
+    const ciphertext = Buffer.concat([prefix, byte, suffix, target]);
+    // console.log('ciphertext', ciphertext);
+    // console.log('ciphertext', ciphertext.length);
+    if (apiEndpoint(ciphertext)) {
+      const intermByte = byte.xor(paddingTarget);
+      // console.log('found byte', padding, 'value', intermByte);
+      return intermByte;
+    }
+  }
+  return -1;
 }
