7b8e25fa501926b5a06277bc23d2052cb2fbdaa5
leonh <hello@leonh.fr>
2018-09-28 19:31:38 +0200

    feat: challenges 19 & 20

--- set3/19.js
+++ set3/19.js
@@ -3,4 +3,61 @@ const crypto = require('crypto');
 module.exports = (data) => {
   console.log('Break fixed-nonce CTR mode using substitutions\n');
 
+  const key   = crypto.randomBytes(16);
+  const nonce = Buffer.alloc(8);
+
+  const ciphertexts = data
+    .split('\n')
+    .filter(el => el.length > 0)
+    .map(line => line.base64Decode().ctr(key, nonce));
+
+  const truncLen = ciphertexts.reduce((acc, val) => {
+    return Math.min(acc, val.length);
+  }, Infinity);
+
+  const ciphers    = ciphertexts.map(el => el.slice(0, truncLen));
+  const transposed = ciphers.transposeBuffers();
+
+  const keystream = [];
+  for (let i = 0; i < truncLen; i++) {
+    keystream.push(transposed[i].breakCTRByte().keyByte);
+  }
+  for (let cipher of ciphertexts) {
+    const plaintext = cipher
+      .slice(0, truncLen)
+      .xor(Buffer.from(keystream))
+      .asciiEncode();
+    console.log(plaintext);
+  }
+};
+
+Buffer.prototype.breakCTRByte = function () {
+  const bestGuess = {
+    score: 0,
+    keyByte: 0
+  };
+  for (let char = 0; char < 256; char++) {
+    const score = this
+      .xor(Buffer.from([char]))
+      .asciiEncode()
+      .scoreText();
+
+    if (score > bestGuess.score) {
+      bestGuess.score   = score;
+      bestGuess.keyByte = char;
+    }
+  }
+  return bestGuess;
+};
+
+Array.prototype.transposeBuffers = function () {
+  const transposed = [];
+  for (let i = 0; i < this[0].length; i++) {
+    let el = Buffer.alloc(0);
+    for (let k = 0; k < this.length; k++) {
+      el = Buffer.concat([el, Buffer.from([this[k][i]])]);
+    }
+    transposed.push(el);
+  }
+  return transposed;
 };
--- set3/20.js
+++ set3/20.js
@@ -3,4 +3,5 @@ const crypto = require('crypto');
 module.exports = (data) => {
   console.log('Break fixed-nonce CTR statistically\n');
 
+  // see challenge 19
 };
